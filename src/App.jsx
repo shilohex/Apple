c8b19320056cc479dfb61733f72079ee7e75e6be
Vivian <ndidiranjo@gmail.com>
2024-03-11 13:48:33 +0100

    Support Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import Home from "./pages/Home";
 import Mac from "./pages/Mac";
 import Ipad from "./pages/Ipad";
+import Support from "./pages/Support";
+import Head from "./pages/Head";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Route path="/" element={<Home />}/> 
       <Route path="/mac" element={<Mac />}/> 
       <Route path="/ipad" element={<Ipad />}/> 
+      <Route path="/support" element={<Support/>}/> 
+      <Route path="/head" element={<Head/>}/>
     </Routes>
    </BrowserRouter>
   );
