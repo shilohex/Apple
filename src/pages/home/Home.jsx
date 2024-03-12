cf0ab5914a65481cf99e2cd2a0b2dbdc0916efbb
Excel oladipupo <shiloholadipupo@gamil.com>
2024-03-12 17:25:21 +0100

    iphone15

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import "./home.css";
 import background2 from "../../assets/imgsec.jpg";
 import background3 from "../../assets/iphone.jpg";
 import sign from "../../assets/greaterthan.svg";
+import background4 from "../../assets/promoiphone.jpg";
 
 const Home = () => {
   return (
@@ -50,6 +51,33 @@ const Home = () => {
                 <p>Learn more</p>
                 <img src={sign} alt="" />
               </div>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div className="david">
+            <div>
+              <img src={background4} alt="" />
+              <div className="imgdiv">
+                <h1> iPhone 15 </h1>
+                <p>New camera. New design. Newphoria</p>
+                <div className="nav">
+                  <p>Learn more</p>
+                  <img src={sign} alt="" />
+                </div>
+              </div>
+            </div>
+            <div>
+              <img src={background4} alt="" />
+            </div>
+          </div>
+          <div className="david">
+            <div>
+              <img src={background4} alt="" />
+            </div>
+            <div>
+              <img src={background4} alt="" />
             </div>
           </div>
         </section>
