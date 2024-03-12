b567b2dd8b63e16b4e0538cdd619c4fb86848a0e
Excel oladipupo <shiloholadipupo@gamil.com>
2024-03-12 14:50:05 +0100

    iphone fearures

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,56 +3,58 @@ import Header from "../../component/Header/Header";
 import backgroundimg from "../../assets/mac.jpg";
 import "./home.css";
 import background2 from "../../assets/imgsec.jpg";
+import background3 from "../../assets/iphone.jpg";
+import sign from "../../assets/greaterthan.svg";
 
 const Home = () => {
   return (
-    <>
+    <div>
       <Header />
-      <section>
-        <div className="mac">
-          <img src={backgroundimg} alt="" />
-          <div className="imgdiv" id="first">
-            <h1> MacBook Pro</h1>
-            <p>Mind blowing. Head-turning.</p>
-            <div>
-              <nav>
-                <a href="Learn more >"> Learn more {">"}</a>
-              </nav>
-            </div>
-          </div>
-        </div>
-      </section>
+      <div id="home">
+        <section>
+          <div className="mac">
+            <img src={backgroundimg} alt="" />
+            <div className="imgdiv" id="first">
+              <h1> MacBook Pro</h1>
+              <p>Mind blowing. Head-turning.</p>
 
-      <section>
-        <div className="mac" id="sec">
-          <img src={background2} alt="" />
-          <div className="imgdiv">
-            <h1> iMac</h1>
-            <p>Packed with more juice.</p>
-            <div>
-              <nav>
-                <a href="Learn more >"> Learn more {">"}</a>
-              </nav>
+              <div className="nav">
+                <p>Learn more</p>
+                <img src={sign} alt="" />
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      <section>
-        <div className="mac" id="sec">
-          <img src={background2} alt="" />
-          <div className="imgdiv">
-            <h1> iMac</h1>
-            <p>Packed with more juice.</p>
-            <div>
-              <nav>
-                <a href="Learn more >"> Learn more {">"}</a>
-              </nav>
+        <section>
+          <div className="mac" id="sec">
+            <img src={background2} alt="" />
+            <div className="imgdiv">
+              <h1> iMac</h1>
+              <p>Packed with more juice.</p>
+              <div className="nav">
+                <p>Learn more</p>
+                <img src={sign} alt="" />
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-    </>
+        </section>
+
+        <section>
+          <div className="mac" id="third">
+            <img src={background3} alt="" />
+            <div className="imgdiv">
+              <h1> iPhone 15 Pro</h1>
+              <p id="st">Titanium. So strong. So light. So Pro</p>
+              <div className="nav">
+                <p>Learn more</p>
+                <img src={sign} alt="" />
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
   );
 };
 
